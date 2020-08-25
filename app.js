907948d7659778e0cbc145455b2e0765778cea49
Malik <malikmukhtar63@gmail.com>
2020-08-25 23:21:18 +0100

    testimonial slider

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ $("#navbar a").on("click", function (event) {
     );
   }
 });
+
+//Slider Effect
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("inside");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+}
